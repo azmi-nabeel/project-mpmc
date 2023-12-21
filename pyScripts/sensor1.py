6186827a4697d6cefc897f71c5793705376dca27
Azmi Nabeel <azmihero158@gmail.com>
2023-12-21 10:22:05 +0530

    added Soil Sensor

--- pyScripts/sensor1.py
+++ pyScripts/sensor1.py
@@ -1,1 +1,20 @@
-print("This is sensor 1")
+import Rpi.GPIO as GPIO
+import time
+
+channel = 21
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN)
+
+
+def callback(channel):
+    if GPIO.input(channel):
+        print("no water detected")
+    else:
+        print("water detected")
+
+
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_callback(channel, callback)
+
+while True:
+    time.sleep(1)
